87a452dd1e7106420095f1a3d65ac1f5885003b9
Jesus-QC <jesusquirocampos@gmail.com>
2022-08-14 14:04:49 +0200

    patch 2 transpiler

--- AudioPlayer/Patches/RunDissonanceAsHost.cs
+++ AudioPlayer/Patches/RunDissonanceAsHost.cs
@@ -1,17 +1,32 @@
-﻿using Dissonance;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Dissonance;
 using Dissonance.Integrations.MirrorIgnorance;
 using Dissonance.Networking;
 using HarmonyLib;
+using NorthwoodLib.Pools;
 
 namespace AudioPlayer.Patches
 {
     [HarmonyPatch(typeof(BaseCommsNetwork<MirrorIgnoranceServer, MirrorIgnoranceClient, MirrorConn, Unit, Unit>), nameof(BaseCommsNetwork<MirrorIgnoranceServer, MirrorIgnoranceClient, MirrorConn, Unit, Unit>.RunAsDedicatedServer))]
     public static class RunDissonanceAsHost
-    {
-        public static bool Prefix(BaseCommsNetwork<MirrorIgnoranceServer, MirrorIgnoranceClient, MirrorConn, Unit, Unit> __instance)
+    { 
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            __instance.RunAsHost(Unit.None, Unit.None);
-            return false;
+            var newInstructions = ListPool<CodeInstruction>.Shared.Rent();
+            
+            newInstructions.AddRange(new CodeInstruction[]
+            {
+                new (OpCodes.Ldarg_0),
+                new (OpCodes.Ldsfld, AccessTools.Field(typeof(Unit),nameof(Unit.None))),
+                new (OpCodes.Ldsfld, AccessTools.Field(typeof(Unit),nameof(Unit.None))),
+                new (OpCodes.Callvirt, AccessTools.Method(typeof(BaseCommsNetwork<MirrorIgnoranceServer, MirrorIgnoranceClient, MirrorConn, Unit, Unit>), nameof(BaseCommsNetwork<MirrorIgnoranceServer, MirrorIgnoranceClient, MirrorConn, Unit, Unit>.RunAsHost)))
+            });
+
+            foreach (var instruction in newInstructions)
+                yield return instruction;
+            
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
         }
     }
 }
